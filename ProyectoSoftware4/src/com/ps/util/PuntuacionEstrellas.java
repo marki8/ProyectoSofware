d77b7099819de729a06d21312c50d966120d767c
Jose <josnick15@gmail.com>
2015-05-04 10:53:35 +0200

    Añadida clase para puntacion estrellas y Splash screen a medias

---
+++ ProyectoSoftware4/src/com/ps/util/PuntuacionEstrellas.java
@@ -0,0 +1,126 @@
+package com.ps.util;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+/**
+ * Clase que implementa una puntuacion de estrellas, es necesario definir una posicion
+ * @author joseangel
+ *
+ */
+public class PuntuacionEstrellas extends JComponent implements MouseMotionListener,MouseListener{
+	
+	private BufferedImage EstrellaVacia,Estrella;
+	private int xpos,ypos,puntuacion;
+	private boolean Seleccionado;
+	
+	public PuntuacionEstrellas(int xpos,int ypos){
+		try {
+			this.setBounds(xpos, ypos, 5*70, 64);
+			this.EstrellaVacia=ImageIO.read(new File("assets/estrella Vacia.png"));
+			this.Estrella=ImageIO.read(new File("assets/estrella.png"));
+			this.puntuacion=0;
+			this.xpos=xpos;
+			this.ypos=ypos;
+			this.Seleccionado=false;
+			this.addMouseListener(this);
+			this.addMouseMotionListener(this);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public int GetPuntuacion(){
+		return this.puntuacion;
+	}
+	/*
+	 * Obtiene la puntuacion dependiendo del punto dado
+	 */
+	private void GetPuntuacion(Point p){
+			//si el punto se encuentra dentro de las estrellas
+		if(ypos<p.getY() && p.getY()<ypos+64){
+			if(xpos<p.getX() && p.getX()<xpos+(35*5)) this.puntuacion=((p.x-xpos)/35)+1;
+			
+		}
+		else this.puntuacion=0;
+		repaint();
+	}
+	
+	private void HacerDibujo(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.scale(0.5, 0.5);
+        for(int i=0;i<this.puntuacion;i++)
+        	g2d.drawImage(this.Estrella,xpos+(i*70),ypos, null);
+        for(int i=this.puntuacion;i<5;i++)
+        	g2d.drawImage(this.EstrellaVacia,xpos+(i*70),ypos, null);
+    }
+	
+	
+	 public void paintComponent(Graphics g) {
+	        super.paintComponent(g);
+	        HacerDibujo(g);
+	  }
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		this.Seleccionado=!this.Seleccionado;
+		System.out.println("Puntucacion selecionada: "+ this.Seleccionado);
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		if(!Seleccionado){
+			Point p= arg0.getPoint();
+			GetPuntuacion(p);
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		if(!Seleccionado){
+			Point p= arg0.getPoint();
+			GetPuntuacion(p);
+		}
+		
+	}
+
+}
---
+++ ProyectoSoftware4/src/com/ps/util/SplashScreen.java
@@ -0,0 +1,238 @@
+package com.ps.util;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Label;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JWindow;
+
+import com.Main.Main;
+
+public class SplashScreen extends JWindow {
+	
+	private BufferedImage logo;
+	private ImageIcon iconoCarga;
+	private MediaTracker tracker;
+	private int duration;
+	private JFrame frame;
+	private JPanel panel;
+	
+	public SplashScreen(int duration){
+		this.duration=duration;
+		this.setLayout(null);
+		this.setBackground(new Color(78, 197, 255));
+		
+		this.setSize(1000, 800);
+		this.setVisible(true);
+		
+		this.frame=new JFrame();
+		//this.frame.setSize(Main.Alto, Main.Ancho);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setVisible(true);
+		this.panel=new JPanel();
+		this.panel.setSize(Main.Alto, Main.Ancho);
+		this.frame.add(this.panel);
+		this.dispose();
+		InitSplash();
+		
+	}
+	
+	public void InitSplash(){
+		LoadImage();	
+		try {Thread.sleep(duration);} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}			
+		LoadLoginData();
+		System.out.println("CArgo opciotndgad");
+	}
+	
+	private void LoadImage(){
+		try {
+			this.logo=ImageIO.read(new File("assets/logotipo.png"));
+			JLabel logolabel= new JLabel(new ImageIcon(this.logo));
+			logolabel.setSize(50,50);
+			this.panel.add(logolabel);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void LoadIcon(){
+		this.iconoCarga=new ImageIcon("assets/loading.gif");
+		this.iconoCarga.setImageObserver(this);
+		JLabel panelaux=new JLabel(this.iconoCarga);
+		
+		
+		panelaux.setHorizontalTextPosition(JLabel.LEFT);
+		panelaux.setVerticalTextPosition(JLabel.BOTTOM);
+	    
+	    
+		this.add(panelaux);
+	}
+	
+	private void LoadLoginData(){
+		
+		/*
+		 * Texto de usuario
+		 */
+		JLabel usuario= new JLabel();
+		usuario.setFont(usuario.getFont().deriveFont(30f));
+		usuario.setText("Usuario: ");
+		usuario.setBackground(Color.BLACK);
+		usuario.setBounds((Main.Ancho/2)+300,(Main.Alto/2)+this.logo.getHeight(), 150, 30);
+		
+		/*
+		 * Panel para introducir el usuario
+		 */
+		JTextField usuarioText = new JTextField(32);
+		usuarioText.setEditable(true);
+		usuarioText.setBounds((Main.Ancho/2)+500,(Main.Alto/2)+this.logo.getHeight(), 150, 30);
+		usuarioText.addKeyListener(new KeyListener() {			
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}			
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				repaint();
+			}			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub	
+			}
+		});
+		
+		/*
+		 * Texto de la contraseña
+		 */
+		JLabel contraseña= new JLabel();
+		contraseña.setFont(contraseña.getFont().deriveFont(30f));
+		contraseña.setText("Contraseña: ");
+		contraseña.setBackground(Color.BLACK);
+		contraseña.setBounds((Main.Ancho/2)+300,(Main.Alto/2)+this.logo.getHeight()+50, 200, 30);
+		
+		/*
+		 * Field para introducir la contraseña
+		 */
+		JTextField contraseñaText = new JTextField(32);
+		contraseñaText.setEditable(true);
+		contraseñaText.setBounds((Main.Ancho/2)+500,(Main.Alto/2)+this.logo.getHeight()+50, 150, 30);
+		contraseñaText.addKeyListener(new KeyListener() {			
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}			
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				repaint();
+			}			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub	
+			}
+		});
+		
+		
+		
+		/*
+		 * Boton aceptar
+		 */
+		
+		JButton botonAceptar=  new JButton("Aceptar");
+		botonAceptar.setFont(botonAceptar.getFont().deriveFont(20f));
+		botonAceptar.setBounds((Main.Ancho/2)+300, (Main.Alto/2)+this.logo.getHeight()+125, 150, 50);
+		botonAceptar.addActionListener(new ActionListener() {
+			
+			//Funcion que se activa cuando se pulsa el boton 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		
+		JButton botonRegistrarse=  new JButton("Registrarse");
+		botonRegistrarse.setFont(botonAceptar.getFont().deriveFont(20f));
+		botonRegistrarse.setBounds((Main.Ancho/2)+500, (Main.Alto/2)+this.logo.getHeight()+125, 150, 50);
+		botonRegistrarse.addActionListener(new ActionListener() {
+			
+			//Funcion que se activa cuando se pulsa el boton 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		/*
+		 * Añadimos los componentes
+		 */
+		this.panel.add(contraseña);
+		this.panel.add(usuario);
+		
+
+		this.panel.add(contraseñaText);
+		this.panel.add(usuarioText);
+		
+		this.panel.add(botonAceptar);
+		this.panel.add(botonRegistrarse);
+	}
+	
+	private void HacerDibujo(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.scale(0.5, 0.5);
+        g2d.drawImage(this.logo,(Main.Ancho/2)+(int)(this.logo.getWidth()*0.15),
+        		(Main.Alto/2)-200, null);
+    }
+	
+	
+	private Font getFont(String name) {
+	    Font font = null;
+	    try {
+			font=Font.createFont(Font.TRUETYPE_FONT, new File(name));
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return font;
+	  }
+
+}
