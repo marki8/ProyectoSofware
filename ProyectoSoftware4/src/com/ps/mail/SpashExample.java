7dad8781de20959e639b38c00baa777dd53d5f6c
Jose <josnick15@gmail.com>
2015-05-05 20:00:06 +0200

    Añadido menu inicio sesion, falta ventana registrar usuario

---
+++ ProyectoSoftware4/src/com/ps/mail/SpashExample.java
@@ -0,0 +1,27 @@
+package com.ps.mail;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JWindow;
+
+import com.ps.util.Login;
+import com.ps.util.SplashScreen;
+
+public class SpashExample {
+	
+	public static final int Ancho=840;
+	public static final int Alto=700;
+	
+	public static void main(String[] args){
+		JWindow ventana= new SplashScreen(1000);
+		
+		JFrame  Login= new JFrame(BorderLayout.CENTER);
+		Login.setSize(Ancho, Alto);
+		Login.add(new Login());
+		Login.setVisible(true);
+		
+	}
+
+}
